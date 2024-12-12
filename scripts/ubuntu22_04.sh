54e7c60397f7b835d97df2e56d5bd0a3ea3a0a68
Chris Johnston <40300884+cj667113@users.noreply.github.com>
2024-12-11 17:38:54 -0800

    Update ubuntu22_04.sh
    Update-Grub and ISCSI modules

--- scripts/ubuntu22_04.sh
+++ scripts/ubuntu22_04.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 sudo ln -sf /lib/systemd/system/multi-user.target /etc/systemd/system/default.target
-sudo apt-get install dracut-core -y
+sudo apt-get install dracut-core dracut-network iscsiuio -y
+echo "Dependencies Installed"
+echo 'add_dracutmodules+="iscsi"' >> /etc/dracut.conf.d/iscsi.conf
+echo "ISCSI Modules Added to Dracut"
 cat /etc/default/grub | grep -v 'GRUB_SERIAL_COMMAND\|GRUB_TERMINAL\|GRUB_CMDLINE_LINUX' > /tmp/grub
 echo 'GRUB_TERMINAL="console"' >> /tmp/grub
 echo 'GRUB_CMDLINE_LINUX="crashkernel=auto ip=dhcp LANG=en_US.UTF-8 console=tty0 console=ttyS0,115200 rd.luks=0 rd.md=0 rd.dm=0 rd.net.timeout.carrier=5 iommu=on systemd.log_level=debug systemd.log_target=kmsg log_buf_len=5M netroot=iscsi:@169.254.0.2::::iqn.2015-02.oracle.boot:uefi iscsi_initiator=iqn.2015- 02.oracle.boot:instance rd.iscsi.param=node.session.timeo.replacement_timeout=6000 net.ifnames=1 nvme_core.shutdown_timeout=10 ipmi_si.tryacpi=0 ipmi_si.trydmi=0 libiscsi.debug_libiscsi_eh=1 loglevel=4 ip=dhcp,dhcp6 rd.net.timeout.dhcp=10 crash_kexec_post_notifiers"' >> /tmp/grub
 cp /tmp/grub /etc/default/grub
-grub2-mkconfig -o /etc/grub2-efi.cfg
+#grub2-mkconfig -o /etc/grub2-efi.cfg
+sudo update-grub
 sudo stty -F /dev/ttyS0 speed 115200
 dmesg | grep console
 sudo systemctl enable getty@ttyS0
