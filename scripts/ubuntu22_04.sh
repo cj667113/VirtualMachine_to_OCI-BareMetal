5a012d4476fe24764d49e84dc8ac16b744320247
Chris Johnston <40300884+cj667113@users.noreply.github.com>
2024-12-11 16:35:39 -0800

    Update ubuntu22_04.sh
    Install dracut-core for ubuntu 22.04

--- scripts/ubuntu22_04.sh
+++ scripts/ubuntu22_04.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sudo ln -sf /lib/systemd/system/multi-user.target /etc/systemd/system/default.target
+sudo apt-get install dracut-core -y
 cat /etc/default/grub | grep -v 'GRUB_SERIAL_COMMAND\|GRUB_TERMINAL\|GRUB_CMDLINE_LINUX' > /tmp/grub
 echo 'GRUB_TERMINAL="console"' >> /tmp/grub
 echo 'GRUB_CMDLINE_LINUX="crashkernel=auto ip=dhcp LANG=en_US.UTF-8 console=tty0 console=ttyS0,115200 rd.luks=0 rd.md=0 rd.dm=0 rd.net.timeout.carrier=5 iommu=on systemd.log_level=debug systemd.log_target=kmsg log_buf_len=5M netroot=iscsi:@169.254.0.2::::iqn.2015-02.oracle.boot:uefi iscsi_initiator=iqn.2015- 02.oracle.boot:instance rd.iscsi.param=node.session.timeo.replacement_timeout=6000 net.ifnames=1 nvme_core.shutdown_timeout=10 ipmi_si.tryacpi=0 ipmi_si.trydmi=0 libiscsi.debug_libiscsi_eh=1 loglevel=4 ip=dhcp,dhcp6 rd.net.timeout.dhcp=10 crash_kexec_post_notifiers"' >> /tmp/grub
