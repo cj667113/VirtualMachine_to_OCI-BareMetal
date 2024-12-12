ad43c11d3de4b4e80bc0b266550f80b33183ae04
Chris Johnston <40300884+cj667113@users.noreply.github.com>
2024-12-12 07:44:34 -0800

    Update ubuntu22_04.sh
    Ubuntu22.04 Fixes

--- scripts/ubuntu22_04.sh
+++ scripts/ubuntu22_04.sh
@@ -1,29 +1,19 @@
 #!/bin/bash
 
 sudo ln -sf /lib/systemd/system/multi-user.target /etc/systemd/system/default.target
-sudo apt-get install dracut-core dracut-network iscsiuio open-iscsi qemu-guest-agent -y
-sudo systemctl enable qemu-guest-agent
-#sudo systemctl start qemu-guest-agent
-sudo systemctl enable iscsid
-#sudo systemctl start iscsid
+sudo apt-get install dracut-core dracut-network *iscsi* qemu-guest-agent -y
 echo "Dependencies Installed"
-echo 'add_dracutmodules+=" iscsi "' >> /etc/dracut.conf.d/iscsi.conf
-echo 'add_drivers+=" virtio_blk virtio_net virtio_scsi "' >> /etc/dracut.conf.d/virtio.conf
-echo "ISCSI Modules Added to Dracut"
 cat /etc/default/grub | grep -v 'GRUB_SERIAL_COMMAND\|GRUB_TERMINAL\|GRUB_CMDLINE_LINUX' > /tmp/grub
 echo 'GRUB_TERMINAL="console"' >> /tmp/grub
 echo 'GRUB_CMDLINE_LINUX=""' >> /tmp/grub
-echo 'GRUB_CMDLINE_LINUX_DEFAULT="console=tty1 console=ttyS0 nvme.shutdown_timeout=10 libiscsi.debug_libiscsi_eh=1 crash_kexec_post_notifiers"' >> /tmp/grub
+echo 'GRUB_CMDLINE_LINUX_DEFAULT="console=tty1 console=ttyS0 nvme.shutdown_timeout=10 libiscsi.debug_libiscsi_eh=1 crash_kexec_post_notifiers netroot=iscsi:@169.254.0.2::::iqn.2015-02.oracle.boot:uefi iscsi_initiator=iqn.2015-02.oracle.boot:instance ip=dhcp"' >> /tmp/grub
 mkdir /etc/iscsi
 echo 'InitiatorName=iqn.2015-02.oracle.boot:uefi' > /etc/iscsi/initiatorname.iscsi
 cp /tmp/grub /etc/default/grub
-#grub2-mkconfig -o /etc/grub2-efi.cfg
 sudo update-grub
 sudo stty -F /dev/ttyS0 speed 115200
 dmesg | grep console
 sudo systemctl enable getty@ttyS0
 sudo systemctl start getty@ttyS0
-for file in $(find /boot -name "vmlinuz-*" -and -not -name "vmlinuz-*rescue*") ; do
-dracut --force --no-hostonly /boot/initramfs-${file:14}.img ${file:14} ; done
 update-initramfs -u
 sudo halt -p
