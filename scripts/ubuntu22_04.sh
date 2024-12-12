3f82b8644f2ddd81af21f61d5e361e5baf37e075
Chris Johnston <40300884+cj667113@users.noreply.github.com>
2024-12-12 08:53:33 -0800

    Update ubuntu22_04.sh
    Ubuntu22.04 Fixes

--- scripts/ubuntu22_04.sh
+++ scripts/ubuntu22_04.sh
@@ -9,7 +9,6 @@ cat /etc/default/grub | grep -v 'GRUB_SERIAL_COMMAND\|GRUB_TERMINAL\|GRUB_CMDLIN
 echo 'GRUB_TERMINAL="console"' >> /tmp/grub
 echo 'GRUB_CMDLINE_LINUX=""' >> /tmp/grub
 echo 'GRUB_CMDLINE_LINUX_DEFAULT="console=tty1 console=ttyS0 nvme.shutdown_timeout=10 libiscsi.debug_libiscsi_eh=1 crash_kexec_post_notifiers netroot=iscsi:@169.254.0.2::::iqn.2015-02.oracle.boot:uefi iscsi_initiator=iqn.2015-02.oracle.boot:instance ip=dhcp"' >> /tmp/grub
-mkdir /etc/iscsi
 echo 'InitiatorName=iqn.2015-02.oracle.boot:uefi' > /etc/iscsi/initiatorname.iscsi
 cp /tmp/grub /etc/default/grub
 sudo update-grub
