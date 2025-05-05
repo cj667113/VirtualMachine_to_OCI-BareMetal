8f613981cf780519303b0fd9b1b5c0a2dde437c0
Chris Johnston <40300884+cj667113@users.noreply.github.com>
2025-05-05 11:12:57 -0700

    Update rhel9.sh

--- scripts/rhel9.sh
+++ scripts/rhel9.sh
@@ -2,7 +2,7 @@
 yum install iscsi-initiator-utils iscsi-initiator-utils-iscsiuio libiscsi udisks2-iscsi -y
 
 sudo ln -sf /lib/systemd/system/multi-user.target /etc/systemd/system/default.target
-read VG LV < <(lvs --noheadings -o vg_name,lv_name | awk 'NR==1 {print $1, $2}')
+read VG LV < <(lvs --noheadings -o vg_name,lv_name | grep rootlv | awk '{print $1, $2}')
 cat /etc/default/grub | grep -v 'GRUB_SERIAL_COMMAND\|GRUB_TERMINAL\|GRUB_CMDLINE_LINUX' > /tmp/grub
 echo 'GRUB_TERMINAL="console"' >> /tmp/grub
 GRUB_CMDLINE_LINUX="ip=dhcp LANG=en_US.UTF-8 console=tty0 console=ttyS0,115200 iommu=on systemd.log_level=debug systemd.log_target=kmsg log_buf_len=5M netroot=iscsi:@169.254.0.2::::iqn.2015-02.oracle.boot:uefi rd.luks=0 rd.md=0 rd.dm=0 rd.lvm.vg=${VG} rd.lvm.lv=${VG}/${LV} rd.lvm=1 root=/dev/mapper/${VG}-${LV} rd.net.timeout.dhcp=10 rd.net.timeout.carrier=5 rd.iscsi.param=node.session.timeo.replacement_timeout=6000 net.ifnames=1 nvme_core.shutdown_timeout=10 ipmi_si.tryacpi=0 ipmi_si.trydmi=0 libiscsi.debug_libiscsi_eh=1 loglevel=4 crash_kexec_post_notifiers crashkernel=1G-64G:448M,64G-:512M rd.iscsi.firmware=1 rd.iscsi.initiator=iqn.2015-02.oracle.boot:instance"
