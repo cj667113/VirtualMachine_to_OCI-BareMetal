74a0e0bdc64739d509e3447fa8e2679855d0e8e0
Chris Johnston <40300884+cj667113@users.noreply.github.com>
2025-05-07 09:34:43 -0700

    Update rocky9.sh

--- scripts/rocky9.sh
+++ scripts/rocky9.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-yum install iscsi-initiator-utils iscsi-initiator-utils-iscsiuio libiscsi udisks2-iscsi -y
-
+yum install iscsi-initiator-utils iscsi-initiator-utils-iscsiuio libiscsi udisks2-iscsi grub2-efi-x64 grub2-efi-x64-modules shim-x64 -y
+#Disk may need to change
+grub2-install /dev/sda
 sudo ln -sf /lib/systemd/system/multi-user.target /etc/systemd/system/default.target
 # grep rootlv may need to be adjusted if the root filesystem is on a different lv_name
-read VG LV < <(lvs --noheadings -o vg_name,lv_name | grep rootlv | awk '{print $1, $2}')
+read VG LV < <(lvs --noheadings -o vg_name,lv_name | grep root | awk '{print $1, $2}')
 cat /etc/default/grub | grep -v 'GRUB_SERIAL_COMMAND\|GRUB_TERMINAL\|GRUB_CMDLINE_LINUX' > /tmp/grub
 echo 'GRUB_TERMINAL="console"' >> /tmp/grub
 GRUB_CMDLINE_LINUX="ip=dhcp LANG=en_US.UTF-8 console=tty0 console=ttyS0,115200 iommu=on systemd.log_level=debug systemd.log_target=kmsg log_buf_len=5M netroot=iscsi:@169.254.0.2::::iqn.2015-02.oracle.boot:uefi rd.luks=0 rd.md=0 rd.dm=0 rd.lvm.vg=${VG} rd.lvm.lv=${VG}/${LV} rd.lvm=1 root=/dev/mapper/${VG}-${LV} rd.net.timeout.dhcp=10 rd.net.timeout.carrier=5 rd.iscsi.param=node.session.timeo.replacement_timeout=6000 net.ifnames=1 nvme_core.shutdown_timeout=10 ipmi_si.tryacpi=0 ipmi_si.trydmi=0 libiscsi.debug_libiscsi_eh=1 loglevel=4 crash_kexec_post_notifiers crashkernel=1G-64G:448M,64G-:512M rd.iscsi.firmware=1 rd.iscsi.initiator=iqn.2015-02.oracle.boot:instance"
